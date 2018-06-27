a09db5c01012dbfe80fae40abb9ff0aecc17f828
moshe0 <qwmoshe@gmail.com>
2018-06-27 16:37:34 +0300

    Ex5

--- src/containers/SendingMessage.tsx
+++ src/containers/SendingMessage.tsx
@@ -29,15 +29,15 @@ class SendingMessage extends React.Component <{}, ISendingMessageState> {
         });
     };
 
-     private handleButtonClick = async() => {
+    private handleButtonClick = async() => {
         if(this.state.inputVal.trim() === '')
             return;
         let m = new Message(0, this.state.inputVal, this.stateStore.get('currentUser').Name, this.stateStore.get('Receiver').Name, moment().format('h:mm:ss'));
         this.setState({inputVal: ''});
 
         await appService.AddMessage(m);
-         StateStore.socket.emit('chat', InitTree.GetSelectedChildrenNames());
-     };
+        StateStore.socket.emit('chat', InitTree.GetSelectedChildrenNames());
+    };
 
     EnterKeyPress = (key : any) => {
         if (key.key === 'Enter') {
@@ -48,23 +48,28 @@ class SendingMessage extends React.Component <{}, ISendingMessageState> {
 
     public render() {
         let inputDisabled = false;
+        let placeholder = 'Type Message...';
         let buttonDisabled = false;
-        if(!this.stateStore.get('currentUser') || InitTree.SelectedType() === 'Not selected')
+        if(!this.stateStore.get('currentUser') || InitTree.SelectedType() === 'Not selected') {
+            placeholder = '';
             inputDisabled = true;
+        }
         else if(InitTree.SelectedType() !== 'User without parent' && InitTree.SelectedType() !== 'User in a parent'){
             let index = InitTree.GetSelectedChildrenNames().find(item => item === StateStore.getInstance().get('currentUser').Name);
-            if (!index)
+            if (!index) {
+                placeholder = '';
                 inputDisabled = true;
+            }
         }
 
         if(this.state.inputVal.trim() === '' || inputDisabled)
-             buttonDisabled = true;
+            buttonDisabled = true;
 
         let btnClass = (buttonDisabled) ? 'buttonDisabled' : 'buttonActive';
 
         return (
             <div className={'SendingMessage'}>
-                <input onKeyUp={this.EnterKeyPress} type='text' className='MessageInput' disabled={inputDisabled} onChange={this.handleInputChange} value={this.state.inputVal} placeholder={'Type Message...'}/>
+                <input onKeyUp={this.EnterKeyPress} type='text' className='MessageInput' disabled={inputDisabled} onChange={this.handleInputChange} value={this.state.inputVal} placeholder={placeholder}/>
                 <button onClick={this.handleButtonClick} className={btnClass} type='button' disabled={buttonDisabled}>
                     <div className="SendingImg"/>
                 </button>
